e00afb81f5495d7f752bc7eb94b2be30281dd855
ltbringer <amresh.venugopal@gmail.com>
2024-02-17 17:13:18 +0000

    refactor: setup to individual installation

--- setup/kind.sh
+++ setup/kind.sh
@@ -1,5 +1,5 @@
-!/bin/bash
-
+#!/bin/bash
+# source: https://kind.sigs.k8s.io/docs/user/quick-start/#installing-from-release-binaries
 # For AMD64 / x86_64 (my case)
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
 # For ARM64
